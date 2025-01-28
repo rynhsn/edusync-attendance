5f0f5a9e7ae9e4f27c2028c21c106e9e94ff3ee7
unknown <riyan.husen@gmail.com>
2025-01-28 23:07:18 +0700

    hapus base_url untuk resources

--- app/Views/attendance.php
+++ app/Views/attendance.php
@@ -14,10 +14,10 @@
 
     <link href="/assets/vendors/libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"> -->
-    <link rel="stylesheet" href="<?= base_url(); ?>assets/vendors/fonts/tabler-icons.css" />
+    <link rel="stylesheet" href="/assets/vendors/fonts/tabler-icons.css" />
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" /> -->
     <link rel="stylesheet" href="/assets/vendors/libs/sweetalert2/sweetalert2.css">
-    <link rel="stylesheet" href="<?= base_url(); ?>assets/vendors/libs/animate-css/animate.css">
+    <link rel="stylesheet" href="/assets/vendors/libs/animate-css/animate.css">
 
     <style>
         body {
